273a739f207fb020baf87f334c7c3eeaf57d35df
Joseph Mancuso <idmann509@gmail.com>
2018-07-21 23:14:22 -0400

    fixed issue with getting teams in settings

--- app/Team.py
+++ app/Team.py
@@ -7,12 +7,19 @@ from app.DraftedPokemon import DraftedPokemon
 class Team(Model):
     __fillable__ = ['name', 'league_id', 'owner_id', 'picture']
 
-    def get_team_pokemon(self, league):
-        return DraftedPokemon \
-               .where('team_id', self.id) \
-               .where('league_id', league.id) \
-               .where_not_null('pokemon_id') \
-               .get()
+    def get_team_pokemon(self, league=False):
+        if league:
+            return DraftedPokemon \
+                .where('team_id', self.id) \
+                .where('league_id', league.id) \
+                .where_not_null('pokemon_id') \
+                .get()
+        else:
+            return DraftedPokemon \
+                .where('team_id', self.id) \
+                .where('league_id', self.league_id) \
+                .where_not_null('pokemon_id') \
+                .get()
     
     @belongs_to('owner_id', 'id')
     def owner(self):
