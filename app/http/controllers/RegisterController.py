cf800f1eb424dc7b942e1668267b12100f70d36e
Joseph Mancuso <idmann509@gmail.com>
2018-07-21 23:03:10 -0400

    register controller update

--- app/http/controllers/RegisterController.py
+++ app/http/controllers/RegisterController.py
@@ -22,20 +22,13 @@ class RegisterController:
         """ Register a new user """
 
         validate = RegisterValidator(Request).register()
-        print(Request.all())
-        print(validate.check(), validate.errors())
-        print('sha1:', hashlib.sha1(Request.input('password').encode('utf-8')).hexdigest())
-
 
         if not validate.check():
             Request.session.flash('validation', json.dumps(validate.errors()))
             return Request.redirect_to('register')
 
-        return
         # register the user
         password = bcrypt_password(Request.input('password'))
-        
-        print('creating password:', password)
 
         auth.AUTH['model'].create(
             name=Request.input('username'),
