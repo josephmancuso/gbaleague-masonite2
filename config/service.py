4ee5e6a9ea635efbb482069a84ec9220982b7e6e
Joseph Mancuso <idmann509@gmail.com>
2018-07-22 19:21:03 -0400

    removed slack stuff

--- config/service.py
+++ config/service.py
@@ -1,13 +1,8 @@
 import os
 
-DRIVER = 'slack'
+DRIVER = 'discord'
 
 DRIVERS = {
-    'slack': {
-        'client': '140404785458.236564549872',
-        'secret': '8fbb7888bf021a27394abc0eb5c78b36',
-        'redirect': 'http://90bebe00.ngrok.io/oauth/slack'
-    },
     'discord': {
         'client': os.getenv('DISCORD_CLIENT'),
         'secret': os.getenv('DISCORD_SECRET'),
