0d67d99c33d4d85da6f16e05b0dfdaeec7eeac9d
Joseph Mancuso <idmann509@gmail.com>
2018-08-26 20:52:46 -0400

    added custom error messaging

--- app/helpers/SentryExceptionHook.py
+++ app/helpers/SentryExceptionHook.py
@@ -1,8 +1,12 @@
 class SentryExceptionHook:
 
     def load(self, container):
-        from raven import Client
-        client = Client(
-            'https://7b9f4b2052ff4a43a689d2597250f8a4:4b435a4c2f7d461b81a356a81477a396@sentry.io/1198997')
         if container.make('Application').DEBUG == 'False':
+            from raven import Client
+            client = Client(
+                'https://7b9f4b2052ff4a43a689d2597250f8a4:4b435a4c2f7d461b81a356a81477a396@sentry.io/1198997')
+            client.user_context({
+                'url': container.make('Request').path,
+                'user': container.make('Request').user().email if container.make('Request').user() else None
+            })
             client.captureException()
