d86541637ec9198c576140f77bc5d89b9a176177
Joseph Mancuso <idmann509@gmail.com>
2018-11-28 12:38:12 -0500

    fixed sentry hook

--- app/helpers/SentryExceptionHook.py
+++ app/helpers/SentryExceptionHook.py
@@ -1,7 +1,9 @@
+from config import application
+
 class SentryExceptionHook:
 
     def load(self, container):
-        if container.make('Application').DEBUG == 'False':
+        if not application.DEBUG:
             from raven import Client
             client = Client(
                 'https://7b9f4b2052ff4a43a689d2597250f8a4:4b435a4c2f7d461b81a356a81477a396@sentry.io/1198997')
