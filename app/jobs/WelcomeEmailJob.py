25ac565b007e6f331d6a28ae4e72444fec7f9170
Joseph Mancuso <idmann509@gmail.com>
2019-02-08 18:06:04 -0500

    fixed email queue job

--- app/jobs/WelcomeEmailJob.py
+++ app/jobs/WelcomeEmailJob.py
@@ -2,11 +2,12 @@
 
 from masonite.queues import Queueable
 from app.notifications import WelcomeNotification
-
+from notifications import Notify
+ 
 class WelcomeEmailJob(Queueable):
 
-    def __init__(self, Notify):
-        self.notify = Notify
+    def __init__(self, notify: Notify):
+        self.notify = notify
 
     def handle(self, email):
         self.notify.mail(WelcomeNotification, to=email)
